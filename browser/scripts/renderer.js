6b3911109ee7c3fdbffa1b23c64a88debdeb1553
Jaakko Manninen <kschzt@gmail.com>
2014-04-10 00:32:36 +0300

    color.js

--- browser/scripts/renderer.js
+++ browser/scripts/renderer.js
@@ -1,18 +1,5 @@
 
-function Color(r, g, b, a)
-{
-	this.rgba = [r, g, b, a || 1.0];
-}
-
-Color.prototype.clone = function(src)
-{
-	var s = src.rgba, d = this.rgba;
-	
-	d[0] = s[0];
-	d[1] = s[1];
-	d[2] = s[2];
-	d[3] = s[3];
-};
+var Color = require('./color').Color;
 
 function TextureSampler(tex)
 {
